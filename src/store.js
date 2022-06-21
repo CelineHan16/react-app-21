0a4ecc7f90f242262421d1a20ba413efaaf667b1
syrgo <syrga.v30@gmail.com>
2022-06-22 00:21:36 +0300

    added new reducers to store.js

--- src/store.js
+++ src/store.js
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
+import albumsSlice from './albumsSlice';
+import photosSlice from './photosSlice';
 import postsSlice from './postsSlice';
+import todosSlice from './todosSlice';
 import usersSlice from "./usersSlice";
 
 const store = configureStore({
   reducer: {
     users: usersSlice,
     posts: postsSlice,
+    albums: albumsSlice,
+    photos: photosSlice,
+    todos: todosSlice
   },
 });
 
