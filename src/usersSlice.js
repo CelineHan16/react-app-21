92e1080598502f9a8c3e152ab9c799f0dfb73abf
syrgo <syrga.v30@gmail.com>
2022-06-22 01:13:20 +0300

    created new const addUser in usersSlice.js

--- src/usersSlice.js
+++ src/usersSlice.js
@@ -4,7 +4,22 @@ import axios from "axios";
 export const loadUsers = createAsyncThunk('users/load', async (arg, thunkAPI) => {
   const response = await axios.get('https://jsonplaceholder.typicode.com/users');
   return response.data;
-})
+});
+
+export const addUser = createAsyncThunk(
+  "users/add", // action name
+  async (name, thunkAPI) => {
+    // action function
+    const response = await axios.post(
+      "https://jsonplaceholder.typicode.com/users",
+      {
+        name: name
+      }
+    );
+
+    return response.data;
+  }
+);
 
 const usersSlice = createSlice({
   name: "users",
@@ -17,6 +32,9 @@ const usersSlice = createSlice({
   extraReducers: {
       [loadUsers.fulfilled]: (state, action) => {
       state.users = action.payload;
+    },
+    [addUser.fulfilled]: (state, action) => {
+      state.users.push(action.payload);
     }
   }
 });
