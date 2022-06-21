0bbf4738ad7497a6840039b7059ddd835d88390a
syrgo <syrga.v30@gmail.com>
2022-06-22 00:20:56 +0300

    created todosSlice.js

---
+++ src/todosSlice.js
@@ -0,0 +1,24 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const loadTodos = createAsyncThunk('todos/load', async (arg, thunkAPI) => {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
+  return response.data;
+})
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState: {
+    todos: [],
+  },
+  reducers: {
+
+  },
+  extraReducers: {
+      [loadTodos.fulfilled]: (state, action) => {
+      state.todos = action.payload;
+    }
+  }
+});
+
+export default todosSlice.reducer;
