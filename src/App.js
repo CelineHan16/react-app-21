3d348bd0ff1be99c6df5be5fede8b0a3458a49a5
syrgo <syrga.v30@gmail.com>
2022-06-21 15:25:19 +0300

    updated react-router-dom in setting.json

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadPosts } from "./postsSlice";
+import { loadUsers } from "./usersSlice";
 
 function App() {
+  const dispatch = useDispatch();
+  const users = useSelector(store => store.users.users);
+  const posts = useSelector(store => store.posts.posts);
+
+  useEffect(() => {
+    dispatch(loadUsers());
+  }, []);
+  useEffect(() => {
+    dispatch(loadPosts());
+  }, []);
+
+  const output = users.map(user => <li key={user.id}>{user.name}</li>)
+  const output2 = posts.map(post => 
+    <article key={post.id}>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+    </article>)
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {output}
+      </ul>
+      <div>
+        {output2}
+      </div>
     </div>
   );
 }
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import postsSlice from './postsSlice';
+import usersSlice from "./usersSlice";
+
+const store = configureStore({
+  reducer: {
+    users: usersSlice,
+    posts: postsSlice,
+  },
+});
+
+export default store;
